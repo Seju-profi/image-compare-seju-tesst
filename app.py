bb3a72ccad2316a708fcd07dd57a503265a0bcdb
Seju-profi <153783316+Seju-profi@users.noreply.github.com>
2023-12-19 19:06:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,22 +10,18 @@ def resize_to_match(image1, image2):
     resized_image1 = cv2.resize(image1, (size_image2[1], size_image2[0]))  # Resize image1
     return resized_image1, image2
 
-
 def correct_rotation(base_image, image_to_correct):
     orb = cv2.ORB_create()
     keypoints1, descriptors1 = orb.detectAndCompute(base_image, None)
     keypoints2, descriptors2 = orb.detectAndCompute(image_to_correct, None)
 
-    # Ensure descriptors are not None
     if descriptors1 is not None and descriptors2 is not None:
         matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
         matches = matcher.match(descriptors1, descriptors2, None)
 
-        # Convert matches to a list if not already (this depends on the OpenCV version)
         if not isinstance(matches, list):
             matches = list(matches)
 
-        # Proceed only if there are enough matches
         if len(matches) > 10:
             matches.sort(key=lambda x: x.distance, reverse=False)
 
@@ -37,32 +33,35 @@ def correct_rotation(base_image, image_to_correct):
                 points2[i, :] = keypoints2[match.trainIdx].pt
 
             h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
-
-            # Ensure a valid homography matrix was found
             if h is not None:
                 height, width, channels = base_image.shape
                 corrected_image = cv2.warpPerspective(image_to_correct, np.linalg.inv(h), (width, height))
                 return corrected_image
 
-    # If descriptors are None or not enough matches, return original image
     return image_to_correct
 
+def preprocess_images_for_comparison(image1, image2):
+    image1, image2 = resize_to_match(image1, image2)
+    image1 = correct_rotation(image2, image1)
+    return image1, image2
 
 def read_image_file(file):
-    # Read image file and convert to RGB format
-    image = Image.open(io.BytesIO(file.read())).convert('RGB')
-    return np.array(image)
+    try:
+        image = Image.open(io.BytesIO(file.read())).convert('RGB')
+        return np.array(image)
+    except Exception as e:
+        st.error(f"An error occurred when reading the image file: {e}")
+        return None
 
 def display_image_info(image_file):
     if image_file is not None:
-        # Reading the image file
         image = Image.open(image_file)
         st.image(image, caption='Uploaded Image', use_column_width=True)
         st.write("Dimensions: ", image.size)
         st.write("File Size: {:.2f} KB".format(image_file.size / 1024))
 
 def main():
-    st.title("TESTTT---Image Comparing App")
+    st.title("Image Comparing App")
 
     col1, col2 = st.columns(2)
 
@@ -82,19 +81,22 @@ def main():
                 image1 = read_image_file(image1_file)
                 image2 = read_image_file(image2_file)
 
-                preprocessed_img1, preprocessed_img2 = preprocess_images_for_comparison(image1, image2)
+                if image1 is not None and image2 is not None:
+                    preprocessed_img1, preprocessed_img2 = preprocess_images_for_comparison(image1, image2)
 
-                # Convert the preprocessed images back to PIL images for comparison
-                preprocessed_img1 = Image.fromarray(preprocessed_img1)
-                preprocessed_img2 = Image.fromarray(preprocessed_img2)
+                    # Convert to PIL images for comparison
+                    preprocessed_img1 = Image.fromarray(preprocessed_img1)
+                    preprocessed_img2 = Image.fromarray(preprocessed_img2)
 
-                # Finding difference
-                diff = ImageChops.difference(preprocessed_img1, preprocessed_img2)
+                    # Finding difference
+                    diff = ImageChops.difference(preprocessed_img1, preprocessed_img2)
 
-                # Display the result
-                st.image(diff, caption='Difference', use_column_width=True)
+                    # Display the result
+                    st.image(diff, caption='Difference', use_column_width=True)
 
-            st.success("Comparison Completed!")
+                    st.success("Comparison Completed!")
+                else:
+                    st.error("Failed to read one or both images.")
         else:
             st.warning("Please upload both images before comparing.")
 
