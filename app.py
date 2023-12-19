53eeaeb4c4b2497bdde5dbe8ecaa31f7cd48cea2
Seju-profi <153783316+Seju-profi@users.noreply.github.com>
2023-12-19 20:45:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 import streamlit as st
-# import PIL.Image as Image
-import PIL.ImageChops as ImageChops
-import cv2
 import numpy as np
+from PIL import Image, ImageChops
+import cv2
 import io
 
-def resize_to_match(image1, image2):
-    size_image2 = image2.shape[:2]  # Get dimensions of image2
-    resized_image1 = cv2.resize(image1, (size_image2[1], size_image2[0]))  # Resize image1
-    return resized_image1, image2
-
-from PIL import Image
-import io
-
+@st.cache(allow_output_mutation=True)
 def read_image_file(file_stream):
     try:
         image = Image.open(io.BytesIO(file_stream.read()))
@@ -24,7 +16,10 @@ def read_image_file(file_stream):
         st.error(f"An error occurred when reading the image file: {e}")
         return None
 
-
+def resize_to_match(image1, image2):
+    size_image2 = image2.shape[:2]  # Get dimensions of image2
+    resized_image1 = cv2.resize(image1, (size_image2[1], size_image2[0]))  # Resize image1
+    return resized_image1, image2
 
 def correct_rotation(base_image, image_to_correct):
     orb = cv2.ORB_create()
@@ -56,26 +51,6 @@ def correct_rotation(base_image, image_to_correct):
 
     return image_to_correct
 
-def preprocess_images_for_comparison(image1, image2):
-    image1, image2 = resize_to_match(image1, image2)
-    image1 = correct_rotation(image2, image1)
-    return image1, image2
-
-def read_image_file(file):
-    try:
-        image = Image.open(io.BytesIO(file.read())).convert('RGB')
-        return np.array(image)
-    except Exception as e:
-        st.error(f"An error occurred when reading the image file: {e}")
-        return None
-
-def display_image_info(image_file):
-    if image_file is not None:
-        image = Image.open(image_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        st.write("Dimensions: ", image.size)
-        st.write("File Size: {:.2f} KB".format(image_file.size / 1024))
-
 def main():
     st.title("Image Comparing App")
 
@@ -83,33 +58,37 @@ def main():
 
     with col1:
         st.header("Upload Image-1")
-        image1_file = st.file_uploader("", type=['jpg', 'png', 'jpeg'], key="image1")
-        display_image_info(image1_file)
+        image1_file = st.file_uploader("Choose an image...", type=['jpg', 'png', 'jpeg'], key="image1")
+        if image1_file is not None:
+            image1 = read_image_file(image1_file)
+            st.image(image1, caption='Uploaded Image 1', use_column_width=True)
 
     with col2:
         st.header("Upload Image-2")
-        image2_file = st.file_uploader("", type=['jpg', 'png', 'jpeg'], key="image2")
-        display_image_info(image2_file)
+        image2_file = st.file_uploader("Choose an image...", type=['jpg', 'png', 'jpeg'], key="image2")
+        if image2_file is not None:
+            image2 = read_image_file(image2_file)
+            st.image(image2, caption='Uploaded Image 2', use_column_width=True)
 
     if st.button('Compare Images'):
         if image1_file is not None and image2_file is not None:
             with st.spinner("Comparing Images..."):
+                image1_file.seek(0)
+                image2_file.seek(0)
                 image1 = read_image_file(image1_file)
                 image2 = read_image_file(image2_file)
-
+                
                 if image1 is not None and image2 is not None:
-                    preprocessed_img1, preprocessed_img2 = preprocess_images_for_comparison(image1, image2)
+                    resized_image1, resized_image2 = resize_to_match(image1, image2)
+                    corrected_image = correct_rotation(resized_image1, resized_image2)
 
                     # Convert to PIL images for comparison
-                    preprocessed_img1 = Image.fromarray(preprocessed_img1)
-                    preprocessed_img2 = Image.fromarray(preprocessed_img2)
+                    corrected_image_pil = Image.fromarray(corrected_image)
+                    resized_image2_pil = Image.fromarray(resized_image2)
 
                     # Finding difference
-                    diff = ImageChops.difference(preprocessed_img1, preprocessed_img2)
-
-                    # Display the result
+                    diff = ImageChops.difference(corrected_image_pil, resized_image2_pil).convert('RGB')
                     st.image(diff, caption='Difference', use_column_width=True)
-
                     st.success("Comparison Completed!")
                 else:
                     st.error("Failed to read one or both images.")
